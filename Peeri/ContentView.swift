90a126c8745f93a9d4b147d881b8e21abe69be0e
Aayush9029 <aayushpokharel36@gmail.com>
2023-05-09 03:31:10 -0400

    Initial Commit

---
+++ Peeri/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Peeri
+//
+//  Created by Aayush Pokharel on 2023-05-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Peeri/PeeriApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeeriApp.swift
+//  Peeri
+//
+//  Created by Aayush Pokharel on 2023-05-09.
+//
+
+import SwiftUI
+
+@main
+struct PeeriApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
